689b123c157e59362072e6a10aa365708c0ebc7d
EvgeniyaNesterkova <miledieva08@gmail.com>
2024-12-26 20:45:32 +0300

    add graceful shutdown

--- cmd/loyalty/main.go
+++ cmd/loyalty/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	run()
+}
+
+func run() {
+	log.Println("Start server ...")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	<-ctx.Done()
+	log.Println("Got stop signal")
+}
